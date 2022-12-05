722cb896acadd930dc302086f135464c8ed4a710
phaeryl <florian.arnaud2019@gmail.com>
2022-12-05 14:18:35 +0100

    fixed duplicate nav

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function loadHtmlContent(page) {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
+
       document.getElementById("container").innerHTML =
       this.responseText;
+      console.log(this.responseText)
 
     }
-    xhttp.open("GET", page);
+    xhttp.open("GET", page + ".html");
     xhttp.send();
   }
+
+
 
