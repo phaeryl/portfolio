51b560d2de801af1f8288681733c99cfe1161586
phaeryl <florian.arnaud2019@gmail.com>
2022-12-05 11:45:26 +0100

    quick update on about.html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function loadHtmlContent(page) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("container").innerHTML =
+      this.responseText;
+      console.log(this.responseText);
+
+    }
+    xhttp.open("GET", page);
+    xhttp.send();
+  }
+
